187566ca430f0e42146fc267fe5c020d7439b015
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-05-31 10:35:35 +0300

    commit tree 0dc84115f891073eabee623992e1c87774f42d98

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function subtract(){
+
+}
+function add(a,b){
+    return a+b;
+}
+add(2,3);
+
+function subtract(a,b){
+    return a-b;
+}
+add(3,2);
+function multiply(a,b){
+    return a*b;
+}
+multiply(2,3);
+function divide(a,b){
+    return a/b;
+}
+divide(2,3);
+
+function increment(n){
+    return n = n+1;
+
+}
+function decrement(n){
+    return n = n-1;
+
+}
+function makeInt(N){
+    return parseInt(N,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
